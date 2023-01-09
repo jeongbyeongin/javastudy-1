fc944d5423fc7257d0a416eff920d061459743bd
gdu-dev <minkt78@naver.com>
2023-01-09 16:48:33 +0900

    2023-01-09

--- 03_Array/src/ex01_one_dim/Ex01_array.java
+++ 03_Array/src/ex01_one_dim/Ex01_array.java
@@ -154,8 +154,40 @@ public class Ex01_array {
 		
 	}
 
+	public static void ex07() {
+		
+		// 배열 초기화
+		int[] arr = {10, 20, 30};
+		
+		// 배열 순회
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+	
+	public static void ex08() {  // 연습
+		
+		String[] weekName = {"토", "일", "월", "화", "수", "목", "금"};
+		
+		int day = 13;
+		
+		System.out.println(day + "일은 " + weekName[day % weekName.length] + "요일이다.");
+		
+	}
+	
+	public static void ex09() {  // 연습
+		
+		String[] season = {"겨울", "봄", "여름", "가을"};
+		
+		int month = 5;
+		
+		System.out.println(month + "월은 " + season[month % 12 / 3] + "이다.");
+		
+	}
+	
 	public static void main(String[] args) {
-		ex06();
+		ex09();
 	}
 
 }
