b36f970be0dd2602ce977d6712b90e23a6fcd7f4
gdu-dev <minkt78@naver.com>
2023-01-12 12:38:58 +0900

    2023-01-12

--- 04_Class/src/practice/MainClass.java
+++ 04_Class/src/practice/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
 		BankAccount myAcc = new BankAccount("010-1111-1111", 50_000);
 		BankAccount yourAcc = new BankAccount("010-9999-9999", 100_000);
 		
-		myAcc.transfer(yourAcc, 50_000);
+		myAcc.transfer(yourAcc, -50_000);
 		
 		myAcc.inquiry();
 		yourAcc.inquiry();
