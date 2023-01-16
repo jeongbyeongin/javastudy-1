5c06cdcca2d90ab9e324a16da2bfcd688d9313c6
gdu-dev <minkt78@naver.com>
2023-01-16 09:56:35 +0900

    2023-01-16

--- 04_Class/src/practice03_Watch/Watch.java
+++ 04_Class/src/practice03_Watch/Watch.java
@@ -15,6 +15,34 @@ public class Watch {
 	}
 	
 	// 메소드
+	public void addHour(int hour) {
+		if(hour <= 0) {
+			return;
+		}
+		this.hour += hour;
+		this.hour %= 24;  // hour를 항상 0 ~ 23의 범위로 만들어 주는 코드
+	}
 	
+	public void addMinute(int minute) {
+		if(minute <= 0) {
+			return;
+		}
+		this.minute += minute;
+		addHour(this.minute / 60);  // 몇 시간이 추가되었는지 처리하는 코드
+		this.minute %= 60;  // minute을 항상 0 ~ 59의 범위로 만들어 주는 코드
+	}
 	
+	public void addSecond(int second) {
+		if(second <= 0) {
+			return;
+		}
+		this.second += second;
+		addMinute(this.second / 60);  // 몇 분이 추가되었는지 처리하는 코드
+		this.second %= 60;  // second을 항상 0 ~ 59의 범위로 만들어 주는 코드
+	}
+	
+	public void see() {
+		System.out.println(hour + "시 " + minute + "분 " + second + "초");
+	}
+
 }
