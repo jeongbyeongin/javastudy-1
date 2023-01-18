5ff2c6c18510e104e1998835e04c049c517d1566
gdu-dev <minkt78@naver.com>
2023-01-18 10:59:50 +0900

    2023-01-18

--- 06_Java_Api/src/ex06_Object/MainClass.java
+++ 06_Java_Api/src/ex06_Object/MainClass.java
@@ -10,6 +10,18 @@ public class MainClass {
 		4. Object 타입으로 저장된 객체는 Object 클래스의 메소드만 호출할 수 있으므로 다시 원래 객체의 타입으로 캐스팅해서 사용해야 한다.
 	*/
 	
+	/*
+		Object 클래스의 주요 메소드
+		1. equals
+			1) 동등 비교할 때 사용하는 메소드
+			2) 동등 비교하는 객체들의 참조값을 비교함(객체의 값을 비교하지 못함)
+			3) 객체의 값으로 동등 비교를 수행하기 위해서는 equals 메소드를 "오버라이드" 해야 함
+		2. toString
+			1) 문자열로 변환할 때 사용하는 메소드
+			2) "객체이름 + @ + 참조값" 형식으로 변환함
+			3) 원하는 형식으로 변경하려면 toString 메소드를 "오버라이드" 해야 함
+	*/
+	
 	public static void ex01() {
 		
 		Object person = new Person();   // Object 클래스는 모든 객체를 저장할 수 있다.
@@ -21,20 +33,33 @@ public class MainClass {
 		
 	}
 	
+	public static void ex02() {
+		
+		// name이 같으면 같은 Person으로 처리하기로 한 시스템
+		
+		Person person1 = new Person("홍길동");
+		Person person2 = new Person("홍길동");
+		
+		if(person1.equals(person2)) {
+			System.out.println("같은 Person이다.");
+		} else {
+			System.out.println("다른 Person이다.");
+		}
+		
+	}
+	
+	public static void ex03() {
+		
+		Person person = new Person("홍길동");
+		
+		System.out.println(person.toString());
+		
+		System.out.println(person);  // toString 메소드는 호출을 생략함  
+
+	}
+	
 	public static void main(String[] args) {
-		ex01();
+		ex03();
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
