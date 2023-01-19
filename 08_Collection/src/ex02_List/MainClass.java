f4c8f911083ad37bd600b1cea053c86379ebcfa1
gdu-dev <minkt78@naver.com>
2023-01-19 15:44:40 +0900

    2023-01-19

--- 08_Collection/src/ex02_List/MainClass.java
+++ 08_Collection/src/ex02_List/MainClass.java
@@ -1,6 +1,7 @@
 package ex02_List;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainClass {
@@ -29,8 +30,46 @@ public class MainClass {
 		
 	}
 	
+	public static void ex02() {
+		
+		// ArrayList 초기화
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+		
+		// 리스트 길이
+		System.out.println("리스트 길이 : " + list.size());
+		
+		// 개별 요소 확인
+		System.out.println("첫 번째 요소 : " + list.get(0));
+		System.out.println("마지막 요소  : " + list.get(list.size() - 1));
+		
+		// 리스트 순회
+		// 모든 요소의 합계 구하기
+		int total = 0;
+		for(int i = 0, length = list.size(); i < length; i++) {
+			total += list.get(i);
+		}
+		System.out.println("모든 요소의 합 : " + total);
+		
+	}
+	
+	public static void ex03() {  // 연습
+		
+		List<User> userList = new ArrayList<User>();
+		
+		for(int i = 0; i < 3; i++) {
+			User user = new User("user", "123456");
+			userList.add(user);
+		}
+		
+		// 아이디만 출력하기
+		for(int i = 0, length = userList.size(); i < length; i++) {
+			System.out.println(userList.get(i).getId());
+		}
+		
+	}
+	
 	public static void main(String[] args) {
-		ex01();
+		ex03();
 	}
 
 }
