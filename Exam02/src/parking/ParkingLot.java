1dcce0fc2372d503ecda65658335c90fa24f799c
gdu-dev <minkt78@naver.com>
2023-01-19 18:48:09 +0900

    2023-01-19

---
+++ Exam02/src/parking/ParkingLot.java
@@ -0,0 +1,40 @@
+package parking;
+
+import java.util.Scanner;
+
+public class ParkingLot {
+
+	private String name;
+	private Car[] cars;
+	private int idx;
+	private Scanner sc;
+	
+	public ParkingLot(String name) {
+		this.name = name;
+		cars = new Car[10];
+		sc = new Scanner(System.in);
+	}
+	
+	public void addCar() {
+		System.out.println("현재 등록된 차량 : " + idx + "대");
+		if(idx == cars.length) {
+			System.out.println("더 이상 차량 등록이 불가능합니다.");
+			return;
+		}
+		System.out.print("차량번호 >>> ");
+		String carNo = sc.next();
+		System.out.print("모델 >>> ");
+		String model = sc.next();
+		cars[idx++] = new Car(carNo, model);
+		System.out.println("차량번호 " + carNo + " 차량이 등록되었습니다.");
+	}
+	
+}
+
+
+
+
+
+
+
+
